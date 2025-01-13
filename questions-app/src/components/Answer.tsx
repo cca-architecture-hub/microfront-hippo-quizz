a7c6c8182987963de1a496540426b534261d91de
Fgarciaf <felipe.garcia-fernandez@capgemini.com>
2025-01-13 16:56:35 +0100

    disable answers

--- questions-app/src/components/Answer.tsx
+++ questions-app/src/components/Answer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button"
 
 type AnswerProps = {
@@ -6,14 +6,28 @@ type AnswerProps = {
   onAnswer: (answer: number) => void;
 };
 
-const Answer: React.FC<AnswerProps> = ({ options, onAnswer }) => (
+const Answer: React.FC<AnswerProps> = ({ options, onAnswer }) => {
+  const [submited, setSubmited] = useState<number | null>(null);
+
+  useEffect(() => {
+    setSubmited(null);
+  }, [options]);
+
+  const handleAnswer = (answerIndex: number) => {   
+    if(submited !== null) return;
+    onAnswer(answerIndex);
+    setSubmited(answerIndex);    
+  };
+
+  return (
   <div className="options">
     {options.map((option, index) => (
-      <Button key={index} onClick={() => onAnswer(index)}>
+      <Button key={index} onClick={() => handleAnswer(index)} disabled={submited !== null && submited !== index} >
         {option}
       </Button>
     ))}
-  </div>
-);
+  </div>)
+};
+
 
 export default Answer;
